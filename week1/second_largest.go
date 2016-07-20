62129b6f3ab44d3631e21a056fb24949d11203cc
rsurulik <ravi.surulikamu@nokia.com>
2016-07-19 19:52:05 -0700

    Second largest with duplicate support at run-time inefficiency

--- week1/second_largest.go
+++ week1/second_largest.go
@@ -7,11 +7,15 @@ import (
 func main() {
 	fmt.Println(secondLargest([]int{5, 7, 3, 8, 9}))
 	fmt.Println(secondLargest([]int{2, 1}))
+	fmt.Println(secondLargest([]int{7, 3, 7, 7, 7, 7, 7, 7}))
+	fmt.Println(secondLargest([]int{7, 3, 1, 5, 6, 8, 4, 2}))
+	fmt.Println(secondLargest([]int{7, 3, 1, 7, 6, 7, 4, 2}))
 }
 
 // Assumes arr has atleast 2 elems
 func secondLargest(arr []int) int {
 	_, losers := largest(arr)
+	fmt.Println(losers)
 	max := losers[0]
 	for _, i := range losers[1:] {
 		if i > max {
@@ -31,6 +35,11 @@ func largest(arr []int) (int, []int) {
 
 	if left > right {
 		return left, append(llosers, right)
+	} else if right > left {
+		return right, append(rlosers, left)
 	}
-	return right, append(rlosers, left)
+
+	// Both left and right are equal, return any but losers
+	// could be in both
+	return left, append(llosers, rlosers...)
 }
--- week1/second_largest_test.go
+++ week1/second_largest_test.go
@@ -13,6 +13,7 @@ func TestSecondLargest(t *testing.T) {
 		{[]int{2, 1}, 1},
 		{[]int{7, 3, 1, 5, 6, 8, 4, 2}, 7},
 		{[]int{7, 3, 1, 7, 6, 7, 4, 2}, 6},
+		{[]int{7, 3, 7, 7, 7, 7, 7, 7}, 3},
 	}
 
 	for _, c := range table {
