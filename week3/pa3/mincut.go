2d2808665dc43796b07028cd5a006832d9617564
rsurulik <ravi.surulikamu@nokia.com>
2016-07-22 18:35:58 -0700

    WIP

---
+++ week3/pa3/mincut.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+type Graph struct {
+	V map[Vertex][]Vertex
+	E map[Edge]int
+}
+
+type Vertex int
+type Edge struct {
+	u, v Vertex
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	g := readGraph("kragerMinCut.txt")
+	fmt.Println(minCut(g))
+}
+
+func minCut(g Graph) (cuts []Edge) {
+	n := len(g)
+	// Run Krager's random contraction for n^2.log(n) trials
+	for t := int(n * n * math.Log(10)); t > 0; t-- {
+		k := randomContraction()
+	}
+	return
+}
+
+func NewGraph() *Graph {
+	return &Graph{}
+}
+
+func (g *Graph) AddEdge(u, v Vertex) {
+	if u > v {
+		u, v = v, u
+	}
+	g.E[Edge{u, v}] = 1
+	g.V[u] = append(g.V[u], v)
+}
+
+func (g *Graph) ContractEdge(e Edge) {
+	// delete g.E[e]
+	// foreach edge in g.V[e.u]
+}
--- week3/pa3/mincut_test.go
+++ week3/pa3/mincut_test.go
@@ -1,13 +1,48 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 var tests = []struct {
 	g    Graph
-	want Edges
-}{}
+	want []Edge
+}{
+	{
+		Graph{
+			1: {2, 3, 4, 7},
+			2: {1, 3, 4},
+			3: {1, 2, 4},
+			4: {1, 2, 3, 5},
+			5: {4, 6, 7, 8},
+			6: {5, 7, 8},
+			7: {1, 5, 6, 8},
+			8: {5, 6, 7},
+		},
+		1,2
+		d1,3
+		1,4
+		1,7
+		2,3
+		2,4
+		3,4
+		4,5
+		5,6
+		5,7
+		5,8
+		6,7
+		6,8
+		7,8
+		[]Edge{{1, 7}, {4, 5}},
+	},
+}
 
 func TestMinCut(t *testing.T) {
+	for _, c := range tests {
+		got := minCut(c.g)
+		if reflect.DeepEqual(c.want, got) == false {
+			t.Errorf("want %v, got %v", c.want, got)
+		}
+	}
 }
